834895d35148046ac840ea7177d185b301a33e2d
sullyschoice <sullyschoice>
2026-02-20 21:10:09 -0500

    Fixes for arm builds

--- src/unreal/install_unreal.sh
+++ src/unreal/install_unreal.sh
@@ -59,6 +59,11 @@ sed -i 's/StartupFullscreen=True/StartupFullscreen=False/' /opt/unreal/System/De
 sed -i 's/UseFullscreen=True/UseFullscreen=False/' /opt/unreal/System/DefaultLinux.ini
 sed -i 's/UseJoystick=False/UseJoystick=True/' /opt/unreal/System/DefaultLinux.ini
 
+
+cp /opt/unreal/System/DefaultLinux.ini /opt/unreal/SystemARM64/
+cp /opt/unreal/System/DefUser.ini /opt/unreal/SystemARM64/
+
+
 chown -R 1000:1000 /opt/unreal
 
 cat >$HOME/Desktop/unreal.desktop <<EOL
